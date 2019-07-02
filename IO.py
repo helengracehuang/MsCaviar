951776e48ae06f7355731a90944b8bbc05a47820
Helen Huang (Xin) <helenhuang@g.ucla.edu>
2019-07-01 17:37:51 -0700

    Update IO.py

--- IO.py
+++ IO.py
@@ -1,7 +1,19 @@
 import sys
+import argparse
 import numpy as np 
 
-#returns set of tuples
+#returns LD matrix (Sigma)
+def read_LD(read_fn):
+    f = open(read_fn,'r')
+    SIGMA = []
+    array = []
+    for line in f:
+        line = line.strip()
+        array = line.split()
+        SIGMA.append(array)
+    return SIGMA
+
+#returns 2*n list of [SNP name, association statistics]
 def read_z(read_fn):
     f = open(read_fn, 'r')
     SNP = []
@@ -11,57 +23,39 @@ def read_z(read_fn):
         SNP.append(array)
     return SNP
 
-#returns LD matrix
-def read_LD(read_fn):
-    f = open(read_fn,'r')
-    mat = []
-    array = []
-    for line in f:
-        line = line.strip()
-        array = line.split()
-        mat.append(array)
-    return mat
-
-#outputs 4 files
-def output(causal_vec, SNP, prob_in_causal,causal_post):
-    f = open("result_set.txt",'w')
-    for i in range(len(causal_vec)):
-        f.write(causal_vec[i] + "\n")
-    f.close()
-
-    u = open("result_post.txt",'w')
-    u.write("        ".join(["SNP_ID","Prob_in_pCausalSet","Causal_Post._Prob"]))
-    u.write("\n")
-    for i in range(len(SNP)):
-    	u.write("        ".join([SNP[i],prob_in_causal[i],causal_post[i]]))
-    	u.write("\n")
-    u.close()
-
-    s = open("result_hist.txt",'w')
-
-
-    
-    v = open("result_log.txt",'w')
-
-
-
 if __name__ == "__main__":
-    causal_vec = ["1","2","3"]
-    read_fn = "test.txt"
-    f = open(read_fn,'r')
-    SNP = []
-    prob = []
-    causal = []
-    for line in f:
-        line = line.strip()
-        array = line.split()
-        SNP.append(array[0])
-        prob.append(array[1])
-        causal.append(array[2])
-    output(causal_vec,SNP,prob,causal)
+    parser = argparse.ArgumentParser(description='CAVIAR is a statistical framework that quantifies the probability of each variant '
+    	'to be causal while allowing with arbitrary number of causal variants.')
+    parser.add_argument('-o', '--out', required=True, dest='output_file',
+                        help='output file name')
+    parser.add_argument('-l', '--ld_file', required=True, dest='ld_file',
+                        help='LD input file')
+    parser.add_argument('-z', '--z_file', required=True, dest='zscore_file',
+                        help='z-score and rsID files')
+    parser.add_argument('-r', '--rho-prob', required=False, dest='pho_probability',
+                        help='set pho probability, default is 0.95')
+    parser.add_argument('-c', '--causal', required=False, dest='M_causal',
+                        help='set the maximum number of causal SNPs, default is 2')
 
+    args = parser.parse_args()
+    O_fn = args.output_file
+    LD_fn = args.ld_file
+    Z_fn = args.zscore_file
 
+    M_SIGMA = read_LD(LD_fn)
+    S_VECTOR = read_z(Z_fn)
 
+    if args.pho_probability:
+        rho_prob = args.pho_probability
+    else:
+        rho_prob = 0.95
 
+    if args.M_causal:
+        MAX_causal = args.M_causal
+    else:
+        MAX_causal = 2
 
-
+    print("rho:", rho_prob)
+    print("max causal:", MAX_causal)
+    print(M_SIGMA)
+    print(S_VECTOR)
