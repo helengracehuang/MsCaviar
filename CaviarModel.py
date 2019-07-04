4aecfcabbd92064d6c728e3a2b3a5f977ed57102
rosie068 <rosemaryhe068@gmail.com>
2019-07-03 17:03:19 -0700

    Update CaviarModel.py

--- CaviarModel.py
+++ CaviarModel.py
@@ -28,4 +28,45 @@ class CaviarModel():
         post.findOptiomalSetGreedy(S_VECTOR, NCP, pcausalSet, rank, rho_prob, O_fn)
 
     def finishUp(self):
+        #print the causal set
+        f = open(O_fn + "_set",'w')
+        for i in range(len(causal_vec)):
+            f.write(causal_vec[i] + "\n")
+        f.close()
+
+        #print each SNP and their posterior probs
+        u = open(O_fn + "post",'w')
+        title1 = "SNP_ID"
+        u.write(title1.ljust(20))
+        title2 = "Prob_in_pCausalSet"
+        u.write(title2.ljust(20))
+        title3 = "Causal_Post._Prob"
+        u.write(title3.ljust(20))
+        u.write("\n")
+
+        for i in range(len(SNP)):
+            u.write(SNP[i].ljust(20))
+            u.write(prob_in_causal[i].ljust(20))
+            u.write(causal_post[i].ljust(20))
+            u.write("\n")
+        u.close()
+
+        #not done
+        #not done
+        #not done
+        #not done
+        #not done
+        #TODO
+        s = open(O_fn + "hist",'w')
         
+        s.close()
+
+        #log file
+        v = open(O_fn + "log",'w')
+        v.close()
+        #not done
+        #not done
+        #not done
+        #not done
+        #not done
+            
