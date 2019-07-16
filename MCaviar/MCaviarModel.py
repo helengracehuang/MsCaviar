08caa189ddb270c682679e81cc1798c1671a9185
Helen Huang (Xin) <helenhuang@g.ucla.edu>
2019-07-16 11:38:11 -0700

    Update MCaviarModel.py

--- MCaviar/MCaviarModel.py
+++ MCaviar/MCaviarModel.py
@@ -1,121 +1,146 @@
-
 import sys
-import numpy as np 
-import MUtil
-from MCaviarModel import MCaviarModel
+import numpy as np
+from numpy import kron
 from MPostCal import MPostCal
-import argparse
-import os
+from MUtil import makePositiveSemiDefinite
 
-def read_LD(read_fn):
-    f = open(read_fn,'r')
-    SIGMA = []
-    array = []
-    for line in f:
-        line = line.strip()
-        array = line.split()
-        SIGMA.append(array)
-    return SIGMA
+#return if string is not in array
+def not_find(arr, str):
+    for i in range(len(arr)):
+        if arr[i] == str:
+            return False
+    return True
 
-#returns 2*n list of [SNP name, association statistics]
-def read_z(read_fn):
-    f = open(read_fn, 'r')
-    SNP_NAME = []
-    S_VECTOR = []
+#remove element in position from name, z_vector, LD_matrix
+def remove(matrix, arr1, arr2, pos):
+    del arr1[pos]
+    del arr2[pos]
+    matrix = np.delete(matrix, (pos), axis = 0)
+    matrix = np.delete(matrix,(pos), axis = 1)
+'''
+def swap(matrix, arr1, arr2, pos1, pos2):
+    arr1[pos1], arr1[pos2] = arr1[pos2], arr1[pos1]
+    arr2[pos1], arr2[pos2] = arr2[pos2], arr2[pos1]
+    matrix[[pos1,pos2]] = matrix[[pos2,pos1]]
+    matrix[:,[pos1,pos2]] = matrix[:,[pos2,pos1]]
+''' 
 
-    for line in f:
-        line = line.strip()
-        array = line.split()
-        SNP_NAME.append(array[0])
-        S_VECTOR.append(array[1])
-    return SNP_NAME, S_VECTOR
+def Msort(index, arr1, arr2, matrix):
+    #names
+    temp_arr1 = np.array([])
+    #zscre
+    temp_arr2 = np.array([])
+    #LD mat
+    temp_mat1 = np.array([])
+    temp_mat2 = np.array([])
 
-#outputs 4 files
-def output(output_file, causal_vec, SNP, prob_in_causal, causal_post):
-    #print the causal set
-    f = open(output_file + "_set",'w')
-    for i in range(len(causal_vec)):
-        f.write(causal_vec[i] + "\n")
-    f.close()
-
-    #print each SNP and their posterior probs
-    u = open(output_file + "post",'w')
-    title1 = "SNP_ID"
-    u.write(title1.ljust(20))
-    title2 = "Prob_in_pCausalSet"
-    u.write(title2.ljust(20))
-    title3 = "Causal_Post._Prob"
-    u.write(title3.ljust(20))
-    u.write("\n")
-
-    for i in range(len(SNP)):
-        u.write(SNP[i].ljust(20))
-        u.write(prob_in_causal[i].ljust(20))
-        u.write(causal_post[i].ljust(20))
-        u.write("\n")
-    u.close()
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description='MCAVIAR is a statistical framework that quantifies the probability of each variant '
-        'to be causal while allowing with arbitrary number of causal variants.')
-    parser.add_argument('-o', '--out', required=True, dest='output_file',
-                        help='output file name')
-    parser.add_argument('-l', '--ld_dir', required=True, dest='ld_dir',
-                        help='LD input directory')
-    parser.add_argument('-z', '--z_dir', required=True, dest='zscore_dir',
-                        help='z-score and rsID directory')
-    parser.add_argument('-r', '--rho-prob', required=False, dest='pho_probability',
-                        help='set pho probability, default is 0.95')
-    parser.add_argument('-c', '--causal', required=False, dest='M_causal',
-                        help='set the maximum number of causal SNPs, default is 2')
-    parser.add_argument('-t', '--heterogeneity', required=False, dest='Tau_squared',
-                        help='set the heterogeneity (t^2) across studies, default is 0.2')
-
+    for i in range(len(index)):
+        np.append(temp_arr1,arr1[index[i]])
+        np.append(temp_arr2,arr2[index[i]])
+        #get row
+        np.append(temp_mat1,matrix[index[i]])
     
-    args = parser.parse_args()
-    O_fn = args.output_file
-    LD_root = args.ld_dir
-    Z_root = args.zscore_dir
-    LD_fn_list = os.listdir(LD_root)
-    Z_fn_list = os.listdir(Z_root)
+    #get column as the rows, then transpose the matrix    
+    for i in range(len(index)):
+        np.append(temp_mat2, temp_mat1[:,index[i]])
 
-    LD_fn = []
-    Z_fn = []
-    SNP_NAME = []
+    temp_mat2.transpose()
 
-    if(len(LD_fn_list) != len(Z_fn_list)):
-        print("Number of files do not match, please try again.")
+    return temp_arr1, temp_arr2, temp_mat2
 
-    for i in range(len(LD_fn_list)):
-        if LD_fn_list[i] != ".DS_Store":
-            LD_fn.append(read_LD(LD_root + "/" + LD_fn_list[i]))
 
-    for i in range(len(Z_fn_list)):
-        if Z_fn_list[i] != ".DS_Store":
-            temp_SNPNAME, temp_Z = read_z(Z_root + "/" + Z_fn_list[i])
-            SNP_NAME.append(temp_SNPNAME)
-            Z_fn.append(temp_Z)
+#return intersection of all arrays in snp_name
+def find_intersection(snp_name):
+    intersect = set(snp_name[0])
+    for i in range(1,len(snp_name)):
+        intersect = intersect.intersection(set(snp_name[i]))
+    print("intersect:", intersect)
+    return list(intersect)
 
-    if args.pho_probability:
-        rho_prob = args.pho_probability
-    else:
-        rho_prob = 0.95
+class MCaviarModel():
+    #M_SIGMA is a vector of sigma matrices
+    def __init__(self, M_SIGMA, SNP_NAME, S_VECTOR, O_fn, MAX_causal, NCP, rho_prob, histFlag, gamma, t_squared):
+        self.histFlag = histFlag
+        self.M_SIGMA = M_SIGMA
+        self.SNP_NAME = SNP_NAME
+        self.MAX_causal = MAX_causal
+        self.NCP = NCP
+        self.rho_prob = rho_prob
+        self.gamma = gamma
+        self.O_fn = O_fn
+        self.t_squared = t_squared
+        self.num_of_studies = len(M_SIGMA)
 
-    if args.M_causal:
-        MAX_causal = args.M_causal
-    else:
-        MAX_causal = 2
-    
-    if args.Tau_squared:
-        t_squared = args.Tau_squared
-    else:
-        t_squared = 0.2
 
-    NCP = 5.2
-    histFlag = True
-    gamma = 0.01
+        # TODO: MIGHT HAVE PROBLEMS WITH studies with different lengths
+        intersect = find_intersection(SNP_NAME)
+        for i in range(len(SNP_NAME)):
+            for j in range(len(SNP_NAME[i])):
+                if not_find(intersect, SNP_NAME[i][j]):
+                    remove(M_SIGMA[i],S_VECTOR[i], SNP_NAME[i], j)
 
-    Mcaviar = MCaviarModel(LD_fn, SNP_NAME, Z_fn, O_fn, MAX_causal, NCP, rho_prob, histFlag, gamma, t_squared)
-    Mcaviar.run()
-    Mcaviar.finishUp()
+
+        SNP_NAME = np.asarray(SNP_NAME)
+        for i in range(len(SNP_NAME)):
+            index = SNP_NAME[i].argsort()
+            SNP_NAME[i], S_VECTOR[i], M_SIGMA[i] = Msort(index, SNP_NAME[i], S_VECTOR[i], M_SIGMA[i])
+
+        #snpCount = the number of SNPs available in ALL Studies. For studies with diffrent number of SNPs, we only get the ones
+        #that are in all the studies. snpCount = len(file with smallest number of SNPs)
+        snpCount = len(SNP_NAME[0])
+        self.pcausalSet = np.zeroes((snpCount,snpCount))
+        self.rank = np.zeros((snpCount,snpCount), dtype = int)
+
+        for i in range(len(S_VECTOR)):
+            for j in range(len(S_VECTOR[i])):
+                S_VECTOR[i][j] = float(S_VECTOR[i][j])
+        self.S_VECTOR = S_VECTOR
+
+        #S_matrix is m by n
+        #S matrix becomes S vector
+        S_LONG_VEC = np.empty()
+        for i in range(len(self.S_VECTOR)):
+            np.concatenate(S_LONG_VEC, self.S_VECTOR[i])
+
+        '''
+        S_MATRIX = np.zeros((snpCount, snpCount))
+        for i in range(len(S_VECTOR)):
+            for j in range(len(S_VECTOR[i])):
+                S_MATRIX[j][i] = S_VECTOR[i][j]
+        self.S_MATRIX = S_MATRIX'''
+
+        for i in range(snpCount):
+            for j in range(snpCount):
+                if(abs(float(S_LONG_VEC[i*snpCount + j]) > NCP)):
+                    NCP = abs(float(S_LONG_VEC[i*snpCount + j]))
+
+        for i in range(len(M_SIGMA)):
+            makePositiveSemiDefinite(M_SIGMA[i],snpCount)
+
+        BIG_SIGMA = np.zeros((snpCount*num_of_studies, snpCount*num_of_studies))
+        for i in range(len(M_SIGMA)):
+            #this is n by n
+            temp_sigma = np.zeros((num_of_studies*num_of_studies))
+            temp_sigma[i][i] = 1
+
+            temp_sigma = kron(temp_sigma, M_SIGMA[i])
+            BIG_SIGMA = BIG_SIGMA + temp_sigma
+
+        self.post = MPostCal(BIG_SIGMA, S_LONG_VEC, snpCount, MAX_causal, SNP_NAME, gamma, t_squared ,num_of_studies)
+
+    def run(self):
+        (self.post).findOptimalSetGreedy(self.S_MATRIX, self.NCP, self.pcausalSet, self.rank, self.rho_prob, self.O_fn)
+
+    def finishUp(self):
+        #print the causal set
+        f = open(self.O_fn + "_set.txt",'w')
+        for i in range(len(self.pcausalSet)):
+            if self.pcausalSet[i] == 1:
+                f.write(self.SNP_NAME[i] + "\n")
+        f.close()
+
+        fileName = self.O_fn + "_post.txt"
+        (self.post).printPost2File(fileName)
+
+        name = self.O_fn + "_hist.txt"
+        (self.post).printHist2File(name)
