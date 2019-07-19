a1d0bcf80f414dc463cdfa4264cd233843f12118
Rosemary He <rosemaryhe068@gmail.com>
2019-07-19 16:18:42 -0700

    Add files via upload

--- simulate.py
+++ simulate.py
@@ -34,7 +34,6 @@ if __name__ == "__main__":
     parser.add_argument('-z', '--z_file', required=True, dest='zscore_file',
                         help='z-score and rsID files')
 
-
     args = parser.parse_args()
     LD_fn = args.ld_file
     Z_fn = args.zscore_file
@@ -45,16 +44,63 @@ if __name__ == "__main__":
     for i in range(len(LD)):
         LD[i] = list(map(float, LD[i]))
 
+    # for i in range(25):
+    #     for j in range(25):
+    #         LD[i+25][j] = 0
+    #         LD[i][j+25] = 0
 
+    # print(LD)
+
+    # s = open("new_50_LD_2.txt",'w')
+    # for i in range(50):
+    #     for j in range(50):
+    #         s.write(str(LD[i][j]))
+    #         s.write(" ")
+    #     s.write("\n")
+    # s.close()
+
+
+
+    #C is a vector of 0 and 1's
+    #sigmaC is the vector where at causal snp is NCP, 0 else
     #the 10th and 15th SNP are causal
     NEW_C_VEC = np.zeros(len(SNP_NAME))
-    NEW_C_VEC[10] = 5.2
-    NEW_C_VEC[15] = 5.2
+    NEW_C_VEC[21] = 1
+    print('C_VEC: ' + str(NEW_C_VEC))
+    #NEW_C_VEC[15] = 5.2
 
-    MEAN_VEC = np.matmul(NEW_C_VEC, LD)
+    #C = np.zeros(len(SNP_NAME))
+    #C[0] = 1
+    #C[15] = 1
+
+    MEAN_VEC = np.matmul(LD, NEW_C_VEC) * 5.2
+    print('mean_vec: ' + str(MEAN_VEC))
+    #print('mean_vec*20: ' + str(20*MEAN_VEC))
+    #MEAN_VEC = np.multiply(LD, NEW_C_VEC)
+    #print('mean_vec: ' + str(MEAN_VEC))
+    #print('mean_vec: ' + str(20*MEAN_VEC))
+    #MEAN_VEC = np.matmul(MEAN_VEC, C)
+    print("our way: ", MEAN_VEC)
+
     NEW_Z_SCORE = np.random.multivariate_normal(MEAN_VEC, LD)
+    
 
-    f = open("new_50_Z.txt",'w')
+    '''
+    SIGMA_C = np.zeros((len(SNP_NAME),len(SNP_NAME)))
+    SIGMA_C[10][10] = 5.2
+    SIGMA_C[15][15] = 5.2
+
+    VAR = np.matmul(LD, SIGMA_C)
+    VAR = np.matmul(VAR, LD)
+    VAR = VAR + LD
+
+    MEAN_0_VEC = np.zeros(50)
+
+    NEW_Z_SCORE = np.random.multivariate_normal(MEAN_0_VEC, VAR)
+    '''
+    
+
+    f = open("new_50_Z_2.txt",'w')
     for i in range(50):
         f.write(str(i).ljust(20))
         f.write(str(NEW_Z_SCORE[i]).ljust(20))
