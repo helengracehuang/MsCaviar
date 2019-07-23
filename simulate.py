ab162540aeddb66c4df41356abf42636a6adba20
Rosemary He <rosemaryhe068@gmail.com>
2019-07-23 13:56:30 -0700

    Add files via upload

--- simulate.py
+++ simulate.py
@@ -44,43 +44,47 @@ if __name__ == "__main__":
     for i in range(len(LD)):
         LD[i] = list(map(float, LD[i]))
 
-    # for i in range(25):
-    #     for j in range(25):
-    #         LD[i+25][j] = 0
-    #         LD[i][j+25] = 0
+    '''for i in range(25):
+        for j in range(25):
+            LD[i+25][j] = 0
+            LD[i][j+25] = 0
 
-    # print(LD)
-
-    # s = open("new_50_LD_2.txt",'w')
-    # for i in range(50):
-    #     for j in range(50):
-    #         s.write(str(LD[i][j]))
-    #         s.write(" ")
-    #     s.write("\n")
-    # s.close()
-
-
+    s = open("new_50_LD_2.txt",'w')
+    for i in range(50):
+        for j in range(50):
+            s.write(str(LD[i][j]))
+            s.write(" ")
+        s.write("\n")
+    s.close()'''
 
     #C is a vector of 0 and 1's
     #sigmaC is the vector where at causal snp is NCP, 0 else
     #the 10th and 15th SNP are causal
     NEW_C_VEC = np.zeros(len(SNP_NAME))
-    NEW_C_VEC[21] = 1
-    print('C_VEC: ' + str(NEW_C_VEC))
+    
+    
+    #setting all causals to 1
+    NEW_C_VEC[46] = 1
+    NEW_C_VEC[26] = 1
+    #NEW_C_VEC[30] = 1
+
+    #print('C_VEC: ' + str(NEW_C_VEC))
     #NEW_C_VEC[15] = 5.2
 
     #C = np.zeros(len(SNP_NAME))
     #C[0] = 1
     #C[15] = 1
 
-    MEAN_VEC = np.matmul(LD, NEW_C_VEC) * 5.2
-    print('mean_vec: ' + str(MEAN_VEC))
+    tau_2 = 0.1
+    NCP = np.random.normal(5.2,tau_2)
+    MEAN_VEC = np.matmul(LD, NEW_C_VEC) * NCP
+    #print('mean_vec: ' + str(MEAN_VEC))
     #print('mean_vec*20: ' + str(20*MEAN_VEC))
     #MEAN_VEC = np.multiply(LD, NEW_C_VEC)
     #print('mean_vec: ' + str(MEAN_VEC))
     #print('mean_vec: ' + str(20*MEAN_VEC))
     #MEAN_VEC = np.matmul(MEAN_VEC, C)
-    print("our way: ", MEAN_VEC)
+    #print("our way: ", MEAN_VEC)
 
     NEW_Z_SCORE = np.random.multivariate_normal(MEAN_VEC, LD)
     
