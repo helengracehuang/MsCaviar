96506f0a476752fff18321234ae4cd80d9231ccc
Rosemary He <rosemaryhe068@gmail.com>
2019-07-24 11:40:47 -0700

    Add files via upload

--- simulate.py
+++ simulate.py
@@ -2,6 +2,8 @@ import sys
 import numpy as np 
 import argparse
 from math import sqrt
+import random
+import os
 
 def read_LD(read_fn):
     f = open(read_fn,'r')
@@ -26,92 +28,111 @@ def read_z(read_fn):
         S_VECTOR.append(array[1])
     return SNP_NAME, S_VECTOR
 
+def createFolder(directory):
+    """
+    this function can ONLY create if there is no folder already existing, if exist, will NOT overwrite
+    """
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print('Error: creating directory. ' + directory)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='CAVIAR is a statistical framework that quantifies the probability of each variant '
         'to be causal while allowing with arbitrary number of causal variants.')
     parser.add_argument('-l', '--ld_file', required=True, dest='ld_file',
                         help='LD input file')
     parser.add_argument('-z', '--z_file', required=True, dest='zscore_file',
-                        help='z-score and rsID files')
+                         help='z-score and rsID files')
+    parser.add_argument('-c', '--causal', required=False, dest='num_causal',
+                        help='set the number of causal SNPs, default is 2')
+    parser.add_argument('-t', '--heterogeneity', required=False, dest='Tau_squared',
+                        help='set the heterogeneity (t^2) across studies, default is 0.2')
+    parser.add_argument('-n', '--num_of_studies', required=False, dest='num_studies',
+                        help='set the number of studies, default is 3')
+    parser.add_argument('-m', '--non-centrality parameter', required=False, dest='NCP_in',
+                        help='set the NCP used for drawing, default is 5.2')
+
 
     args = parser.parse_args()
     LD_fn = args.ld_file
     Z_fn = args.zscore_file
 
+    if args.num_causal:
+        c = int(args.num_causal)
+    else:
+        c = 2
+
+    if args.Tau_squared:
+        tau_2 = float(args.Tau_squared)
+    else:
+        tau_2 = 0.2
+
+    if args.num_studies:
+        num_of_studies = int(args.num_studies)
+
+    if args.NCP_in:
+        NCP = int(args.NCP_in)
+    else:
+        NCP = 5.2
+
     LD = read_LD(LD_fn)
     SNP_NAME, S_VEC = read_z(Z_fn)
-
     for i in range(len(LD)):
         LD[i] = list(map(float, LD[i]))
+    snpCount = len(SNP_NAME)
 
-    '''for i in range(25):
-        for j in range(25):
-            LD[i+25][j] = 0
-            LD[i][j+25] = 0
+    createFolder('./true_causal/')
+    createFolder('./Z_test/')
+    createFolder('./LD_test/')
 
-    s = open("new_50_LD_2.txt",'w')
-    for i in range(50):
-        for j in range(50):
-            s.write(str(LD[i][j]))
-            s.write(" ")
+    causal_vec = random.sample(range(snpCount), c)
+    z_path = os.getcwd() + '/true_causal/'
+    s_name = z_path + "true_causal_set.txt"
+    s = open(s_name,'w')
+    for i in range(len(causal_vec)):
+        s.write(str(causal_vec[i]))
         s.write("\n")
-    s.close()'''
+    s.close()
 
-    #C is a vector of 0 and 1's
-    #sigmaC is the vector where at causal snp is NCP, 0 else
-    #the 10th and 15th SNP are causal
-    NEW_C_VEC = np.zeros(len(SNP_NAME))
-    
-    
-    #setting all causals to 1
-    NEW_C_VEC[46] = 1
-    NEW_C_VEC[26] = 1
-    #NEW_C_VEC[30] = 1
+    for k in range(num_of_studies):
+        #C is a vector of 0 and 1's
+        #sigmaC is the vector where at causal snp is NCP, 0 else
+        NEW_C_VEC = np.zeros(len(SNP_NAME))
+        #     NEW_C_VEC[46] = 1
+        #     NEW_C_VEC[26] = 1
+        #     NEW_C_VEC[30] = 1
+        #     NEW_C_VEC[41] = 1
+        #     NEW_C_VEC[32] = 1
 
-    #print('C_VEC: ' + str(NEW_C_VEC))
-    #NEW_C_VEC[15] = 5.2
+        for i in range(len(causal_vec)):
+            NEW_C_VEC[causal_vec[i]] = 1
 
-    #C = np.zeros(len(SNP_NAME))
-    #C[0] = 1
-    #C[15] = 1
+        #change NCP
+        NCP = np.random.normal(NCP,tau_2)
+        MEAN_VEC = np.matmul(LD, NEW_C_VEC) * NCP
 
-    tau_2 = 0.1
-    NCP = np.random.normal(5.2,tau_2)
-    MEAN_VEC = np.matmul(LD, NEW_C_VEC) * NCP
-    #print('mean_vec: ' + str(MEAN_VEC))
-    #print('mean_vec*20: ' + str(20*MEAN_VEC))
-    #MEAN_VEC = np.multiply(LD, NEW_C_VEC)
-    #print('mean_vec: ' + str(MEAN_VEC))
-    #print('mean_vec: ' + str(20*MEAN_VEC))
-    #MEAN_VEC = np.matmul(MEAN_VEC, C)
-    #print("our way: ", MEAN_VEC)
+        NEW_Z_SCORE = np.random.multivariate_normal(MEAN_VEC, LD)
 
-    NEW_Z_SCORE = np.random.multivariate_normal(MEAN_VEC, LD)
-    
+        z_path = os.getcwd() + '/Z_test/'
+        f_name = z_path + 'z_file_' + str(k) + '.txt'
+        f = open(f_name,'w')
+        for i in range(snpCount):
+            f.write(str(i).ljust(20))
+            f.write(str(NEW_Z_SCORE[i]).ljust(20))
+            f.write("\n")
+        f.close()
 
-    '''
-    SIGMA_C = np.zeros((len(SNP_NAME),len(SNP_NAME)))
-    SIGMA_C[10][10] = 5.2
-    SIGMA_C[15][15] = 5.2
-
-    VAR = np.matmul(LD, SIGMA_C)
-    VAR = np.matmul(VAR, LD)
-    VAR = VAR + LD
-
-    MEAN_0_VEC = np.zeros(50)
-
-    NEW_Z_SCORE = np.random.multivariate_normal(MEAN_0_VEC, VAR)
-    '''
-    
-
-    f = open("new_50_Z_2.txt",'w')
-    for i in range(50):
-        f.write(str(i).ljust(20))
-        f.write(str(NEW_Z_SCORE[i]).ljust(20))
-        f.write("\n")
-    f.close()
-
-
+        l_path = os.getcwd() + '/LD_test/'
+        m_name = l_path + 'ld_file_' + str(k) + '.txt'
+        m = open(m_name,'w')
+        for i in range(snpCount):
+            for j in range(snpCount):
+                m.write(str(LD[i][j]) + ' ')
+            m.write("\n")
+        m.close()
 
 
 
